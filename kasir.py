0070d0e47da8fff6c99b1e4a305b6630268c3993
Moch Dziran Al Fadzri <141558649+Zirxn@users.noreply.github.com>
2024-02-29 11:28:47 +0700

    Add files via upload

---
+++ kasir.py
@@ -0,0 +1,9 @@
+rokok, minyak, gula, saldo, diskon = 20000, 25000, 11000, 100000, 10000
+total_belanja = rokok+minyak+gula
+totaldiskon = total_belanja-diskon
+kembalian = saldo-totaldiskon
+
+print("ucup pergi ke warung dan membeli rokok = ",rokok,", minyak =",minyak," & gula = ",gula)
+print("total belanja ucup = ",total_belanja,", ucup membayar sebesar = ",saldo)
+print("ucup mendapat diskon dari belanjaannya sebesar",diskon, "total belanja ucup sekarang = ",totaldiskon)
+print("jadi kembalian ucup = ",kembalian)
